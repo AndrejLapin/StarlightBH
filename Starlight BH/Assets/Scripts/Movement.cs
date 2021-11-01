a57c667bfcefc8b3d8d6d290d320415b62ec5cbe
DESKTOP-LH76H01\Andrej <lapin.andrej1@gmail.com>
2021-11-01 13:33:16 +0200

    small commit

--- Starlight BH/Assets/Scripts/Movement.cs
+++ Starlight BH/Assets/Scripts/Movement.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 10f;
+    static const string HORIZONTAL_AXIS_NAME = "Horizontal";
+    static const string VERTICAL_AXIS_NAME = "Vertical";
+
+    RigidBody2D myRigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // frame rate independant update
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float horizontalThrow = CrossPlatformInputManager.GetAxis(HORIZONTAL_AXIS_NAME);
+        float verticalThrow = CrossPlatformInputManager.GetAxis(VERTICAL_AXIS_NAME);
+
+        Vector3 playerVelocity = new Vector3(horizontalThrow * moveSpeed, 0, verticalThrow * moveSpeed); // need to get absolute vector
+        
+        myRigidBody.velocity = playerVelocity;
     }
 }
